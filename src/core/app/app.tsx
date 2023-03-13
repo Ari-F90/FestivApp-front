1b8706a5a5bc304ee39b2dc99c48e595fbed5288
Ariadna Frias <ariadna.812@gmail.com>
2023-03-13 17:14:01 +0100

    Add app test and tsx file

---
+++ src/core/app/app.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/header/header";
+
+function App() {
+  return (
+    <div className="App">
+      <span hidden>Learn</span>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
