ed8da577e6252ecd74f53454e06b3b461e72d189
Ariadna Frias <ariadna.812@gmail.com>
2023-03-15 22:58:46 +0100

    Update app ts file

--- src/core/app/app.tsx
+++ src/core/app/app.tsx
@@ -1,12 +1,10 @@
 import { Header } from "../components/header/header";
-import { Register } from "../components/register/register";
 
 function App() {
   return (
     <div className="App">
       <span hidden>Learn</span>
       <Header></Header>
-      <Register></Register>
     </div>
   );
 }
