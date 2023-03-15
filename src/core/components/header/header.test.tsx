453e89776be8bd81db8061b6cd63a2abb8e8f084
Ariadna Frias <ariadna.812@gmail.com>
2023-03-15 10:17:23 +0100

    Update header test

--- src/core/components/header/header.test.tsx
+++ src/core/components/header/header.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "../../store/store.js";
+import { store } from "../../store/store";
 import { Header } from "./header";
 
 describe("Given Header component", () => {
