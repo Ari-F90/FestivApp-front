3b9e174f359e520a8c85d2402bfe0523fc9aff28
Ariadna Frias <ariadna.812@gmail.com>
2023-03-15 11:51:38 +0100

    Update reducer files

---
+++ src/features/users/reducer/users.actions.creator.ts
@@ -0,0 +1,9 @@
+import { createAction } from "@reduxjs/toolkit";
+import { usersActions } from "./users.actions.types";
+import { User } from "../models/user";
+
+export const loadCreator = createAction<User[]>(usersActions.load);
+export const loadOneCreator = createAction<User>(usersActions.loadOne);
+export const addCreator = createAction<User>(usersActions.add);
+export const updateCreator = createAction<User>(usersActions.update);
+export const deleteCreator = createAction<User["id"]>(usersActions.delete);
