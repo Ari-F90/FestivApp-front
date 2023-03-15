39195ad6d2d65f2a15ec39344a28c81ab56d5f2e
Ariadna Frias <ariadna.812@gmail.com>
2023-03-15 17:42:50 +0100

    Update header files

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -3,7 +3,7 @@ import styles from "./header.module.scss";
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1> FestivApp</h1>
+      <h1>FestivApp</h1>
     </header>
   );
 }
