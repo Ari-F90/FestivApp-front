31d44b3ebcd2e04f525bca46f324e4cf000c3b08
Ariadna Frias <ariadna.812@gmail.com>
2023-03-17 17:09:13 +0100

    Update about page test

--- src/core/components/pages/about-page/about-page.test.tsx
+++ src/core/components/pages/about-page/about-page.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import AboutPage from "./about-page";
 
-jest.mock("./home-page");
+jest.mock("./about-page");
 describe("Given the home page", () => {
   describe("When it is render", () => {
     test('Then it should render "Home Page"', () => {
