295b31b7821138027d2293c81a6edd79f41557cc
Ariadna Frias <ariadna.812@gmail.com>
2023-03-18 18:44:43 +0100

    Update store ts file

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { festivalReducer } from "../../features/festivals/reducer/festivals.reducer";
 import { userReducer } from "../../features/users/reducer/users.slice";
 
 export const store = configureStore({
-  reducer: { users: userReducer },
+  reducer: { users: userReducer, festivals: festivalReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
