f3ac7e9af2f9ffa2510c9d1b73f873227f119c91
Ariadna Frias <ariadna.812@gmail.com>
2023-03-19 13:54:17 +0100

    Add about page files

--- src/core/components/pages/about-page/about-page.tsx
+++ src/core/components/pages/about-page/about-page.tsx
@@ -3,12 +3,14 @@ import styles from "./about-page.module.scss";
 export default function AboutPage() {
   return (
     <>
-      <h2 className={styles.about__title}>About us</h2>
-      <p>
-        Welcome to our community! Get the chance to visit our app and start
-        creating festivals and connect with other people. We are sure you will
-        be able to know other music fans with the same music interests.
-      </p>
+      <section className={styles.aboutpage}>
+        <h2 className={styles.aboutpage__title}>About us</h2>
+        <p className={styles.aboutpage__text}>
+          Welcome to our community! Get the chance to visit our app and start
+          creating festivals and connect with other people. We are sure you will
+          be able to know other music fans with the same music interests.
+        </p>
+      </section>
     </>
   );
 }
