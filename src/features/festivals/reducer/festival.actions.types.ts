71b11f74b3fdf1067f41c1aa2e67630bdb73cdcd
Ariadna Frias <ariadna.812@gmail.com>
2023-03-19 12:05:01 +0100

    Update delete files

--- src/features/festivals/reducer/festival.actions.types.ts
+++
@@ -1,7 +0,0 @@
-export const festivalsActions = {
-  load: "festivals@load",
-  loadOne: "festivals@loadOne",
-  add: "festivals@add",
-  update: "festivals@update",
-  delete: "festivals@delete",
-};
