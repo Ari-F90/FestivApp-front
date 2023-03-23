a22fb01d6cfb7ed672fafc7cee9079fae460e4ab
Ariadna Frias <ariadna.812@gmail.com>
2023-03-23 12:48:24 +0100

    Move firebase files

--- src/core/components/firebase/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAG26-EPH7gDFlA-niP0hT-8Hr5fAp6pDw",
-  authDomain: "festivapp-1e8e5.firebaseapp.com",
-  projectId: "festivapp-1e8e5",
-  storageBucket: "festivapp-1e8e5.appspot.com",
-  messagingSenderId: "1077004575916",
-  appId: "1:1077004575916:web:3c844abeca13d4b5b3eb09",
-};
