67a65bd8ea2da88a1820da1204622017202f597f
Ariadna Frias <ariadna.812@gmail.com>
2023-03-25 14:46:35 +0100

    Update festival list files

--- src/core/components/festivalList/festivalList.tsx
+++ src/core/components/festivalList/festivalList.tsx
@@ -1,15 +1,23 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
+
 import { Link } from "react-router-dom";
 
 import { useFestivals } from "../../../features/festivals/hooks/use.festivals";
 import { Festival } from "../../../features/festivals/models/festival";
 import { FestivalApiRepo } from "../../../features/festivals/services/repository/festival.repo";
+
 import { Card } from "../card/card";
 import styles from "./festivalList.module.scss";
 
 export function FestivalList() {
   const repo = useMemo(() => new FestivalApiRepo(), []);
-  const { festivals } = useFestivals(repo);
+  const { festivals, loadFestivals } = useFestivals(repo);
+
+  useEffect(() => {
+    loadFestivals();
+  }, [loadFestivals]);
+
+  const setPage = (pageChange: number) => loadFestivals(pageChange);
   return (
     <>
       <div className={styles.festivals_flex}>
@@ -34,12 +42,18 @@ export function FestivalList() {
           <Card festival={item} key={item.id}></Card>
         ))}
       </div>
-      <button>
-        <img src="../../../../img/prev.png" alt="prev-page"></img>
-      </button>
-      <button>
-        <img src="../../../../img/next.png" alt="next-page"></img>
-      </button>
+      <div className={styles.containerbuttons}>
+        <div className={styles.prev}>
+          <button className={styles.prevbutton} onClick={() => setPage(-1)}>
+            <img src="../../../../img/prev.png" alt="prev-page"></img>
+          </button>
+        </div>
+        <div className={styles.next}>
+          <button className={styles.nextbutton} onClick={() => setPage(+1)}>
+            <img src="../../../../img/next.png" alt="next-page"></img>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
