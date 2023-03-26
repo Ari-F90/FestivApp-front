45142c1e5b0ce93fd470c0aa67614cca151564f7
Ariadna Frias <ariadna.812@gmail.com>
2023-03-26 12:51:15 +0200

    Update logout files

---
+++ src/core/components/logout/logout.tsx
@@ -0,0 +1,21 @@
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+
+import { useUsers } from "../../../features/users/hooks/use.users";
+import { UserApiRepo } from "../../../features/users/services/user.api.repo";
+import styles from "./logout.module.scss";
+export default function Logout() {
+  const repo = useMemo(() => new UserApiRepo(), []);
+
+  const { logoutUser } = useUsers(repo);
+  const dispatch = useDispatch();
+  return (
+    <button className={styles.logout} onClick={logoutUser}>
+      <img
+        className={styles.imglogout}
+        src="../../../../img/logout.png"
+        alt="logout"
+      ></img>
+    </button>
+  );
+}
