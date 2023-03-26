ddd24e91b6466d529a0cfccd955aa116ca73f676
Ariadna Frias <ariadna.812@gmail.com>
2023-03-26 17:02:42 +0200

    Update user hook and reducer

--- src/features/users/reducer/users.slice.ts
+++ src/features/users/reducer/users.slice.ts
@@ -19,6 +19,7 @@ export const userSlice = createSlice({
   reducers: {
     register(state, action: PayloadAction<User>) {
       state.users = [...state.users, action.payload];
+      state.userLogged = action.payload;
     },
     login(state, action: PayloadAction<User>) {
       state.userLogged = action.payload;
