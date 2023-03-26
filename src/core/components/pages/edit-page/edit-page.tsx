84f64a36cf3737fb128a96905fd2869384773b6f
Ariadna Frias <ariadna.812@gmail.com>
2023-03-26 18:22:16 +0200

    Update edit page

--- src/core/components/pages/edit-page/edit-page.tsx
+++ src/core/components/pages/edit-page/edit-page.tsx
@@ -5,7 +5,7 @@ export default function EditFormPage() {
   return (
     <>
       <section className={styles.editform}>
-        <h2 className={styles.editform__title}>Update a new festival</h2>
+        <h2 className={styles.editform__title}>Update a festival</h2>
         <Form></Form>
       </section>
     </>
