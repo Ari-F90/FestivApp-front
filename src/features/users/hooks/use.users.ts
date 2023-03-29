dd06fb381ff11087cb349c7209eaf4e56ee1ebf8
Ariadna Frias <ariadna.812@gmail.com>
2023-03-29 12:46:01 +0200

    Update users hook

--- src/features/users/hooks/use.users.ts
+++ src/features/users/hooks/use.users.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
+
 import { AppDispatch, RootState } from "../../../core/store/store";
 import { User } from "../models/user";
 
